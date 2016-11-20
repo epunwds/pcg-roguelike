6f05130114125d100505f07deac391fae17925ae
BugDeveloper <a.losev@innopolis.ru>
2016-11-20 23:51:43 +0300

    Fixed texture values

--- game/core/src/com/pcg/roguelike/world/GameWorld.java
+++ game/core/src/com/pcg/roguelike/world/GameWorld.java
@@ -101,8 +101,8 @@ public class GameWorld {
 
         /* Texture generation */
         for (int i = 0; i < TEXTURE_VARIATIONS; i++) {
-            groundTiles[i] = new TextureRegion(texGen.generateTexture(Color.BROWN, Color.GREEN, TEXTURE_ANTIALIASING));
-            wallTiles[i] = new TextureRegion(texGen.generateTexture(Color.WHITE, Color.BLACK, TEXTURE_ANTIALIASING));
+            groundTiles[i] = new TextureRegion(texGen.generateTexture(Color.LIGHT_GRAY, Color.DARK_GRAY, TEXTURE_ANTIALIASING));
+            wallTiles[i] = new TextureRegion(texGen.generateTexture(Color.GRAY, Color.BLACK, TEXTURE_ANTIALIASING));
         }
 
         /* Creating TiledMap and filling it with tiles corresponding to the blueprint */
